c890548b86df84518d200c9a17e3ac77804a2ca3
Dharsan <139860049+dharsanjilla@users.noreply.github.com>
2024-06-08 20:07:05 +0530

    Update views.py

--- breweries/views.py
+++ breweries/views.py
@@ -80,6 +80,17 @@ def signup(request):
         return render(request, 'signup.html', {'form': form})
 
 
-def login(request):
-        return render(request, 'login.html')
+def login(request,user):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('search_breweries')
+    else:
+        form = UserCreationForm()
+    return render(request, 'login.html', {'form': form})
 
